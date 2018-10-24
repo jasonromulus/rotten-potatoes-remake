3e4f36294fa30fd437ba60b59839aa03b3bbb2cb
jasonromulus <pumcome@me.com>
2018-10-24 14:55:04 -0700

    updated routes!

--- app.js
+++ app.js
@@ -5,9 +5,16 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// OUR MOCK ARRAY OF PROJECTS
+let reviews = [
+  { title: "High Horse", movieTitle: "Kasey Musgraves" },
+  { title: "Issa Photoshoot", movieTitle: "Star" }
+]
+
+// INDEX
 app.get('/', (req, res) => {
-    res.render('home', { msg: 'Handlebars are Cool!' });
-  })
+  res.render('reviews-index', { reviews: reviews });
+})
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
