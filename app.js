e46482d2805236eabef83d13f171619bba1918c3
jasonromulus <pumcome@me.com>
2018-10-28 16:23:22 -0700

    user reviews are now a thing we can create

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require('express')
 const app = express()
 var exphbs = require('express-handlebars');
-
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/rotten-potatoes');
+const bodyParser = require('body-parser');
 
 const Review = mongoose.model('Review', {
   title: String,
+  description: String,
   movieTitle: String
 });
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
+//EVERYTHING BELOW IS ROUTES!
 // INDEX
 app.get('/', (req, res) => {
   Review.find()
@@ -24,6 +27,21 @@ app.get('/', (req, res) => {
     .catch(err => {
       console.log(err);
     })
+})
+
+// NEW
+app.get('/reviews/new', (req, res) => {
+  res.render('reviews-new', {});
+})
+
+// CREATE
+app.post('/reviews', (req, res) => {
+  Review.create(req.body).then((review) => {
+    console.log(review);
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
 })
 
 app.listen(3000, () => {
