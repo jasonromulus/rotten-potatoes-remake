73514b66f57716ae7625f8be76e336a86f27ea17
jasonromulus <pumcome@me.com>
2018-10-29 05:56:12 -0700

    Added some bootstrap!

--- test/test-reviews.js
+++ test/test-reviews.js
@@ -1,35 +1,35 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const server = require('../app');
-const should = chai.should();
-const Review = require('../models/review');
+// const chai = require('chai');
+// const chaiHttp = require('chai-http');
+// const server = require('../app');
+// const should = chai.should();
+// const Review = require('../models/review');
 
-chai.use(chaiHttp);
+// chai.use(chaiHttp);
 
-// tell mocha you want to test Reviews (this string is taco)
-describe('Reviews', ()  => {
-    // make taco name for the test
-    it('should index ALL reviews on / GET', (done) => {
-      // use chai-http to make a request to your server
-      chai.request(server)
-          // send a GET request to root route
-          .get('/')
-          // wait for response
-          .end((err, res) => {
-            // check that the response status is = 200 (success)
-            res.should.have.status(200);
-            // check that the response is a type html
-            res.should.be.html;
-            // end this test and move onto the next.
-            done();
-          });
-    });
+// // tell mocha you want to test Reviews (this string is taco)
+// describe('Reviews', ()  => {
+//     // make taco name for the test
+//     it('should index ALL reviews on / GET', (done) => {
+//       // use chai-http to make a request to your server
+//       chai.request(server)
+//           // send a GET request to root route
+//           .get('/')
+//           // wait for response
+//           .end((err, res) => {
+//             // check that the response status is = 200 (success)
+//             res.should.have.status(200);
+//             // check that the response is a type html
+//             res.should.be.html;
+//             // end this test and move onto the next.
+//             done();
+//           });
+//     });
 
-  // TEST NEW
-  // TEST CREATE
-  // TEST SHOW
-  // TEST EDIT
-  // TEST UPDATE
-  // TEST DELETE
-});
+//   // TEST NEW
+//   // TEST CREATE
+//   // TEST SHOW
+//   // TEST EDIT
+//   // TEST UPDATE
+//   // TEST DELETE
+// });
 
