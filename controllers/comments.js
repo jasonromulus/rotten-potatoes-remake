ee6a47402356ff2b01fa6fc0cc8132129f751ff8
jasonromulus <pumcome@me.com>
2018-11-03 20:11:08 -0700

    Finished Part One!

--- controllers/comments.js
+++ controllers/comments.js
@@ -11,5 +11,14 @@ app.post('/reviews/comments', (req, res) => {
     });
   });
 
+  // DELETE
+app.delete('/reviews/comments/:id', function (req, res) {
+    console.log("DELETE comment")
+    Comment.findByIdAndRemove(req.params.id).then((comment) => {
+      res.redirect(`/reviews/${comment.reviewId}`);
+    }).catch((err) => {
+      console.log(err.message);
+    })
+  })
 
 module.exports = app;
