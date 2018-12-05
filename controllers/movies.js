9eb86e6e6ea98163b3484470c859ef7cf3d5e423
jasonromulus <pumcome@me.com>
2018-12-04 16:40:34 -0800

    added youtube

--- controllers/movies.js
+++ controllers/movies.js
@@ -6,18 +6,25 @@ module.exports = (app) => {
   app.get('/', (req, res) => {
     moviedb.miscNowPlayingMovies().then(response => {
       // console.log(response)
+      if(req.header('Content-Type') == 'application/json'){return res.send({movies:response})}
       res.render('movies-index', { movies: response.results });
     }).catch(console.error)
   });
 
-  router.get('/movies/:id', (req, res) => {
-    moviedb.movieInfo({ id: req.params.id }).then(movie => {
-      moviedb.movieTrailers({ id: req.params.id }).then(videos => {
-        movie.trailer_youtube_id = videos.youtube[0].source
-        console.log('VIDEOS.TRAILER_YOUTUBE_ID', videos.trailer_youtube_id)
-  
-        res.render('movies-show', { movie: movie });
-      }).catch(console.error);
+app.get('/movies/:id', (req, res) => {
+  moviedb.movieInfo({ id: req.params.id }).then(movie => {
+    res.render('movies-show', { movie: movie });
+  }).catch(console.error)
+});
+
+router.get('/movies/:id', (req, res) => {
+  moviedb.movieInfo({ id: req.params.id }).then(movie => {
+    moviedb.movieTrailers({ id: req.params.id }).then(videos => {
+      movie.trailer_youtube_id = videos.youtube[0].source
+      console.log('VIDEOS.TRAILER_YOUTUBE_ID', videos.trailer_youtube_id)
+
+      res.render('movies-show', { movie: movie });
     }).catch(console.error);
-  })
+  }).catch(console.error);
+});
 }
