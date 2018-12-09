7117723c85d0625acdf69000ac6b6027e58e6963
jasonromulus <pumcome@me.com>
2018-12-08 21:27:09 -0800

    can't get youtube to work for me

--- controllers/movies.js
+++ controllers/movies.js
@@ -2,7 +2,7 @@ const MovieDb = require('moviedb-promise');
 const moviedb = new MovieDb('23f232628c0802c2d23d52e6054496cb');
 
 //Index
-module.exports = (app) => {
+module.exports = (app) => { 
   app.get('/', (req, res) => {
     moviedb.miscNowPlayingMovies().then(response => {
       // console.log(response)
@@ -17,14 +17,14 @@ app.get('/movies/:id', (req, res) => {
   }).catch(console.error)
 });
 
-router.get('/movies/:id', (req, res) => {
-  moviedb.movieInfo({ id: req.params.id }).then(movie => {
-    moviedb.movieTrailers({ id: req.params.id }).then(videos => {
-      movie.trailer_youtube_id = videos.youtube[0].source
-      console.log('VIDEOS.TRAILER_YOUTUBE_ID', videos.trailer_youtube_id)
+// router.get('/movies/:id', (req, res) => {
+//   moviedb.movieInfo({ id: req.params.id }).then(movie => {
+//     moviedb.movieTrailers({ id: req.params.id }).then(videos => {
+//       movie.trailer_youtube_id = videos.youtube[0].source
+//       console.log('VIDEOS.TRAILER_YOUTUBE_ID', videos.trailer_youtube_id)
 
-      res.render('movies-show', { movie: movie });
-    }).catch(console.error);
-  }).catch(console.error);
-});
+//       res.render('movies-show', { movie: movie });
+//     }).catch(console.error);
+//   }).catch(console.error);
+// });
 }
